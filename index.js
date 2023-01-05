dfd97dab9e76c1cecab27954e70908421aefd748
Andrew Abercrombie <34100163+AndrewAbercrombie@users.noreply.github.com>
2023-01-05 01:19:29 -0800

    Added optional timeout pram

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const net = require('net');
 
-async function portScan(ip, port) {
+async function portScan(ip, port, timeout = 3000) {
   return new Promise((resolve, reject) => {
     const socket = new net.Socket();
+
+    socket.setTimeout(timeout);
+    socket.on('timeout', () => {
+      resolve(false);
+      socket.destroy();
+    });
 
     socket.on('error', (error) => {
       resolve(false);
